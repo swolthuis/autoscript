b5926c653e6d4f472049af6c7982133d2f0d06bd
swolthuis <58694372+swolthuis@users.noreply.github.com>
2021-06-15 10:41:38 +0200

    save

--- auto.sh
+++ auto.sh
@@ -63,7 +63,7 @@ sudo apt-get install apt-transport-https ca-certificates gnupg lsb-release -y
 if test -f "/etc/docker"; then
   printf "docker is al geinstalleerd"
 else
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
